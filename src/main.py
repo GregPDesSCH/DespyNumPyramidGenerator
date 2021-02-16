a3b2d1ae14eb0e2c1c81aac8f394589a017a7799
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-02-15 21:01:06 -0500

    Adds validation checks for user input and repetition to reenter as required

--- src/main.py
+++ src/main.py
@@ -11,12 +11,39 @@
 
 """
 
+def numberIsAWholeNumber(rawNumber):
+    try:
+        int(rawNumber)
+        return True
+    except ValueError:
+        return False
+
 def getNumberOfLinesFromUser():
-    numberOfLines = int(input("Number of lines to print out for pyramids? "))
+    rawNumberOfLines = ""
+    numberOfLines = 0
+
+    while len(rawNumberOfLines) == 0 or numberOfLines < 3 or numberOfLines > 80 or not numberIsAWholeNumber(rawNumberOfLines):
+        rawNumberOfLines = input("Number of lines to print out for pyramids? ")
+        if len(rawNumberOfLines) == 0:
+            print("ERROR - Input must not be empty.")
+        elif not numberIsAWholeNumber(rawNumberOfLines):
+            print("ERROR - Input must be a whole positive number.")
+        else:
+            numberOfLines = int(rawNumberOfLines)
+            if numberOfLines < 3 or numberOfLines > 80:
+                print("ERROR - Number of lines must be in the range [3-80].")
+        
     return numberOfLines
 
 def getCharacterToPrint():
-    characterToPrint = input('Character to print pyramids? ')
+    characterToPrint = ""
+    while len(characterToPrint) != 1:
+        characterToPrint = input('Character to print pyramids? ')
+        if len(characterToPrint) == 0:
+            print("ERROR - Input must not be empty.")
+        elif len(characterToPrint) > 1:
+            print("ERROR - Input must be exactly one character long.")
+
     return characterToPrint
 
 def main():
