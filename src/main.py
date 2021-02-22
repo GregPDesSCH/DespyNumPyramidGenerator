0c62240f219b76ba3fdbd45b64be4f914b9b25a7
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-02-21 21:38:03 -0500

    Adds an initial selection interface with an option menu

--- src/main.py
+++ src/main.py
@@ -11,6 +11,11 @@
 
 """
 
+def printCommandMenu():
+    print("Pick an option from one of the following:", "1 - Left Pyramid", "2 - Right Pyramid", "3 - Full Pyramid",
+        "4 - Upside Down Left Pyramid", "5 - Upside Down Right Pyramid", "6 - Upside Down Pyrmaid (Mirrored)",
+        "7 - All Options", "8 - Quit", sep="\n")
+
 def numberIsAWholeNumber(rawNumber):
     try:
         int(rawNumber)
@@ -46,34 +51,66 @@ def getCharacterToPrint():
 
     return characterToPrint
 
+
+def selectPrintCommand():
+    rawPrintCommand = ""
+    printCommand = 0
+    while len(rawPrintCommand) != 1 or printCommand < 1 or printCommand > 8 or not numberIsAWholeNumber(rawPrintCommand):
+        printCommandMenu()
+        rawPrintCommand = input('Select option [1-8]: ')
+        if len(rawPrintCommand) == 0:
+            print("ERROR - Input must not be empty.")
+        elif not numberIsAWholeNumber(rawPrintCommand):
+            print("ERROR - Input must be a whole positive number.")
+        else:
+            printCommand = int(rawPrintCommand)
+            if printCommand < 1 or printCommand > 8:
+                print("ERROR - Command must be in the range [1-8].")
+    
+    return printCommand
+
 def main():
-    numberOfLines = getNumberOfLinesFromUser()
-    characterToPrint = getCharacterToPrint()
+    while True:
+        printCommand = selectPrintCommand()
 
-    # Left Pyramid
-    for lineIndex in range(numberOfLines):
-        print("".ljust(lineIndex + 1, characterToPrint).ljust(numberOfLines))
+        if printCommand == 8:
+            break
 
-    # Right Pyramid
-    for lineIndex in range(numberOfLines):
-        print("".ljust(lineIndex + 1, characterToPrint).rjust(numberOfLines))
+        numberOfLines = getNumberOfLinesFromUser()
+        characterToPrint = getCharacterToPrint()
+        
 
-    # Full Pyramid
-    for lineIndex in range(numberOfLines):
-        print("".ljust(lineIndex + 1, characterToPrint).rjust(numberOfLines) + "".rjust(lineIndex + 1, characterToPrint).ljust(numberOfLines))
+        if printCommand == 1 or printCommand == 7:
+            # Left Pyramid
+            for lineIndex in range(numberOfLines):
+                print("".ljust(lineIndex + 1, characterToPrint).ljust(numberOfLines))
 
-    # Upside Down Left Pyramid
-    for lineIndex in range(numberOfLines):
-        print("".ljust(numberOfLines - (lineIndex), characterToPrint).ljust(numberOfLines))
+        if printCommand == 2 or printCommand == 7:
+            # Right Pyramid
+            for lineIndex in range(numberOfLines):
+                print("".ljust(lineIndex + 1, characterToPrint).rjust(numberOfLines))
 
-    # Upside Down Right Pyramid
-    for lineIndex in range(numberOfLines):
-        print("".rjust(numberOfLines - (lineIndex), characterToPrint).rjust(numberOfLines))
+        if printCommand == 3 or printCommand == 7:
+            # Full Pyramid
+            for lineIndex in range(numberOfLines):
+                print("".ljust(lineIndex + 1, characterToPrint).rjust(numberOfLines) + \
+                    "".rjust(lineIndex + 1, characterToPrint).ljust(numberOfLines))
 
-    # Upside Down Pyramid
-    for lineIndex in range(numberOfLines):
-        print("".ljust(numberOfLines - (lineIndex), characterToPrint).ljust(numberOfLines) + \
-            "".rjust(numberOfLines - (lineIndex), characterToPrint).rjust(numberOfLines))
+        if printCommand == 4 or printCommand == 7:
+            # Upside Down Left Pyramid
+            for lineIndex in range(numberOfLines):
+                print("".ljust(numberOfLines - (lineIndex), characterToPrint).ljust(numberOfLines))
+
+        if printCommand == 5 or printCommand == 7:
+            # Upside Down Right Pyramid
+            for lineIndex in range(numberOfLines):
+                print("".rjust(numberOfLines - (lineIndex), characterToPrint).rjust(numberOfLines))
+
+        if printCommand == 6 or printCommand == 7:
+            # Upside Down Pyramid (Mirrored)
+            for lineIndex in range(numberOfLines):
+                print("".ljust(numberOfLines - (lineIndex), characterToPrint).ljust(numberOfLines) + \
+                    "".rjust(numberOfLines - (lineIndex), characterToPrint).rjust(numberOfLines))
 
 if __name__ == "__main__":
     main()
