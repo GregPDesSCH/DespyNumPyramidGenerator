6b265fbe76b6f68a6083fc7bf8e978215b452e9c
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-03-15 20:10:50 -0400

    Introduces the numberOfCharactersToPrintIsTooHigh condition in the conditions module

--- src/conditions.py
+++ src/conditions.py
@@ -17,3 +17,7 @@ def numberIsAWholeNumber(rawNumber):
         return True
     except ValueError:
         return False
+
+def numberOfCharactersToPrintIsTooHigh(printCommand, numberOfLines, stepWidth):
+    return numberOfLines * stepWidth > 160 or (printCommand == 3 or printCommand == 4 or printCommand == 7 or 
+        printCommand == 8 or printCommand == 9) and 2 * numberOfLines * stepWidth > 160
