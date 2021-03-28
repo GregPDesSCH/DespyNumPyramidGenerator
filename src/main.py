4146ac2be74f0be0a4b32f04acd9e75147b68a28
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-03-28 17:22:32 -0400

    Breaks the long if-else decision tree in the main loop to three sections with two functions

--- src/main.py
+++ src/main.py
@@ -16,6 +16,22 @@ from number_triangles import printNumberTrianglePyramids
 from sequences import printSequencePyramids
 from simple import printSimplePyramids
 
+def _selectSequence(sequenceIndex):
+    if sequenceIndex == 2:
+        printSequencePyramids("fibonacci")
+    elif sequenceIndex == 3:
+        printSequencePyramids("triangle")
+    elif sequenceIndex == 4:
+        printSequencePyramids("alternatingBits")
+
+def _selectNumberTriangle(sequenceIndex):
+    if sequenceIndex == 5:
+        printNumberTrianglePyramids("pascal")
+    elif sequenceIndex == 6:
+        printNumberTrianglePyramids("euler")
+    elif sequenceIndex == 7:
+        printNumberTrianglePyramids("catalan")
+
 def main():
     print("DespyNumPyramidGenerator\nby Gregory Desrosiers\n")
 
@@ -24,18 +40,10 @@ def main():
 
         if sequenceIndex == 1:
             printSimplePyramids()
-        elif sequenceIndex == 2:
-            printSequencePyramids("fibonacci")
-        elif sequenceIndex == 3:
-            printSequencePyramids("triangle")
-        elif sequenceIndex == 4:
-            printSequencePyramids("alternatingBits")
-        elif sequenceIndex == 5:
-            printNumberTrianglePyramids("pascal")
-        elif sequenceIndex == 6:
-            printNumberTrianglePyramids("euler")
-        elif sequenceIndex == 7:
-            printNumberTrianglePyramids("catalan")
+        elif sequenceIndex >= 2 and sequenceIndex <= 4:
+            _selectSequence(sequenceIndex)
+        elif sequenceIndex >= 5 and sequenceIndex <= 7:
+            _selectNumberTriangle(sequenceIndex)
         if sequenceIndex == 8:
             break
 
