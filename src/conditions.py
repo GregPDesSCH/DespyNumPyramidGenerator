981abcf1492a142d7c3bb3f7f79d2ca4ff636522
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-04-06 20:50:41 -0400

    Adds line breaks in the conditions module for readability

--- src/conditions.py
+++ src/conditions.py
@@ -13,12 +13,14 @@
 
 _maximumCharactersInOneLine = 160
 
+
 def numberIsAWholeNumber(rawNumber):
     try:
         int(rawNumber)
         return True
     except ValueError:
         return False
+
 
 def numberOfCharactersToPrintIsTooHigh(printCommand, numberOfLines, stepWidth):
     return numberOfLines * stepWidth > _maximumCharactersInOneLine or (printCommand == 3 or printCommand == 4 or printCommand == 7 
