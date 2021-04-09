b4ad3c7d569e0c704bbf1258a7464017fad22a95
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-04-08 22:22:24 -0400

    Adds comments in the conditions module to understand the code

--- src/conditions.py
+++ src/conditions.py
@@ -15,6 +15,7 @@ _MAXIMUM_CHARACTERS_IN_ONE_LINE = 160
 
 
 def numberIsAWholeNumber(rawNumber):
+    """Checks if the input is a whole number or not by parsing."""
     try:
         int(rawNumber)
         return True
@@ -23,5 +24,6 @@ def numberIsAWholeNumber(rawNumber):
 
 
 def numberOfCharactersToPrintIsTooHigh(printCommand, numberOfLines, stepWidth):
+    """Checks if the required number of characters to print out at most is over the console width limit."""
     return numberOfLines * stepWidth > _MAXIMUM_CHARACTERS_IN_ONE_LINE or (printCommand == 3 or printCommand == 4 or printCommand == 7
             or printCommand == 8 or printCommand == 9) and 2 * numberOfLines * stepWidth > _MAXIMUM_CHARACTERS_IN_ONE_LINE
