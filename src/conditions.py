a948ee7a7ff14491d9864dc0cd3d61b034caf8dd
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-04-10 15:14:43 -0400

    Updates the comments in the conditions module

--- src/conditions.py
+++ src/conditions.py
@@ -16,7 +16,15 @@ _MAXIMUM_CHARACTERS_IN_ONE_LINE = 160
 
 
 def numberIsAWholeNumber(rawNumber):
-    """Checks if the input is a whole number or not by parsing."""
+    """
+    Checks if the input is a whole number or not by parsing.
+
+    Params:
+    rawNumber - A string holding the user's raw input (which may be a number)
+
+    Returns:
+    Flag representing whether the input is a whole number or not.
+    """
     try:
         int(rawNumber)
         return True
@@ -25,6 +33,17 @@ def numberIsAWholeNumber(rawNumber):
 
 
 def numberOfCharactersToPrintIsTooHigh(printCommand, numberOfLines, stepWidth):
-    """Checks if the required number of characters to print out at most is over the console width limit."""
+    """
+    Checks if the required number of characters to print out at most is over the console width limit.
+    (only for simple pyramids and pyramids involving sequences)
+
+    Params:
+    printCommand -
+    numberOfLines -
+    stepWidth -
+
+    Returns:
+    Flag representing whether there's at most more than 160 characters to print in a single line or not.
+    """
     return numberOfLines * stepWidth > _MAXIMUM_CHARACTERS_IN_ONE_LINE or (printCommand == 3 or printCommand == 4 or printCommand == 7
             or printCommand == 8 or printCommand == 9) and 2 * numberOfLines * stepWidth > _MAXIMUM_CHARACTERS_IN_ONE_LINE
