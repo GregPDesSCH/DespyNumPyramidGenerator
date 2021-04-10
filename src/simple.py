ac41fd6c1134b475cb02abd24bcab7c809022f3f
Gregory Desrosiers <gregorydesrosiers667@gmail.com>
2021-04-10 12:52:41 -0400

    Adds comments in the simple module to understand the code

--- src/simple.py
+++ src/simple.py
@@ -22,6 +22,7 @@ from inputs import (
 )
 
 
+# Constants for different pyramid orientation options
 _OPTION_TO_PRINT_UPSIDE_PYRAMID_ALIGNED_LEFT = 1
 _OPTION_TO_PRINT_UPSIDE_PYRAMID_ALIGNED_RIGHT = 2
 _OPTION_TO_PRINT_FULL_UPSIDE_PYRAMID = 3
@@ -35,6 +36,12 @@ _OPTION_TO_GO_BACK_TO_PREVIOUS_MENU = 10
 
 
 def getCharacterToPrint():
+    """
+    Reads in user input for selecting the character to fill the pyramid with.
+
+    Returns:
+    characterToPrint - User's selection of a character.
+    """
     characterToPrint = ""
 
     while len(characterToPrint) != 1 and not inputIsGoBackCommand(characterToPrint):
@@ -52,57 +59,60 @@ def getCharacterToPrint():
 
 
 def printPyramid(printCommand, numberOfLines, characterToPrint, stepWidth):
+    """Prints the pyramid out to the console."""
+
     if numberOfCharactersToPrintIsTooHigh(printCommand, numberOfLines, stepWidth):
+        # Only if any of the lines in the pyramid to print has too many characters
         print(getErrorMessagePrefix() + "Maximum number of characters to be printed on the screen is 160. Please enter again.")
         return
 
-    # Left Pyramid
     if printCommand == _OPTION_TO_PRINT_UPSIDE_PYRAMID_ALIGNED_LEFT or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid with right angle on bottom left
         for lineIndex in range(numberOfLines):
             pyramidSegment = "".ljust(stepWidth * (lineIndex + 1), characterToPrint).ljust(stepWidth * numberOfLines)
             print(pyramidSegment)
 
-    # Right Pyramid
     if printCommand == _OPTION_TO_PRINT_UPSIDE_PYRAMID_ALIGNED_RIGHT or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid with right angle on bottom right
         for lineIndex in range(numberOfLines):
             pyramidSegment = "".ljust(stepWidth * (lineIndex + 1), characterToPrint).rjust(stepWidth * numberOfLines)
             print(pyramidSegment)
 
-    # Full Pyramid
     if printCommand == _OPTION_TO_PRINT_FULL_UPSIDE_PYRAMID or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid with base on the bottom side and made up of two joined triangles
         for lineIndex in range(numberOfLines):
             pyramidLeftSegment = "".ljust(stepWidth * (lineIndex + 1), characterToPrint).rjust(stepWidth * numberOfLines)
             pyramidRightSegment = "".rjust(stepWidth * (lineIndex + 1), characterToPrint).ljust(stepWidth * numberOfLines)
             print(pyramidLeftSegment + pyramidRightSegment)
 
-    # Silhouette Pyramid
     if printCommand == _OPTION_TO_PRINT_FULL_UPSIDE_SILHOUETTE_PYRAMID or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid is a silhouette made up of spaces with filled characters outside it, with base on bottom
         for lineIndex in range(numberOfLines):
             pyramidLeftSegment = "".ljust(stepWidth * (numberOfLines - (lineIndex)), characterToPrint).ljust(stepWidth * numberOfLines)
             pyramidRightSegment = "".rjust(stepWidth * (numberOfLines - (lineIndex)), characterToPrint).rjust(stepWidth * numberOfLines)
             print(pyramidLeftSegment + pyramidRightSegment)
 
-    # Upside Down Left Pyramid
     if printCommand == _OPTION_TO_PRINT_UPSIDE_DOWN_PYRAMID_ALIGNED_LEFT or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid with right angle on top left
         for lineIndex in range(numberOfLines):
             pyramidSegment = "".ljust(stepWidth * (numberOfLines - (lineIndex)), characterToPrint).ljust(stepWidth * numberOfLines)
             print(pyramidSegment)
 
-    # Upside Down Right Pyramid
     if printCommand == _OPTION_TO_PRINT_UPSIDE_DOWN_PYRAMID_ALIGNED_RIGHT or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid with right angle on top right
         for lineIndex in range(numberOfLines):
             pyramidSegment = "".rjust(stepWidth * (numberOfLines - (lineIndex)), characterToPrint).rjust(stepWidth * numberOfLines)
             print(pyramidSegment)
 
-    # Upside Down Pyramid
     if printCommand == _OPTION_TO_PRINT_FULL_UPSIDE_DOWN_PYRAMID or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid with base on the top side and made up of two joined triangles
         for lineIndex in range(numberOfLines):
             pyramidLeftSegment = "".ljust(stepWidth * (numberOfLines - (lineIndex)), characterToPrint).rjust(stepWidth * numberOfLines)
             pyramidRightSegment = "".rjust(stepWidth * (numberOfLines - (lineIndex)), characterToPrint).ljust(stepWidth * numberOfLines)
             print(pyramidLeftSegment + pyramidRightSegment)
 
-    # Silhouette Pyramid (Upside Down)
     if printCommand == _OPTION_TO_PRINT_FULL_UPSIDE_DOWN_SILHOUETTE_PYRAMID or printCommand == _OPTION_TO_PRINT_ALL_PYRAMID_TYPES:
+        # Pyramid is a silhouette made up of spaces with filled characters outside it, with base on top
         for lineIndex in range(numberOfLines):
             pyramidLeftSegment = "".ljust(stepWidth * (lineIndex + 1), characterToPrint).ljust(stepWidth * numberOfLines)
             pyramidRightSegment = "".rjust(stepWidth * (lineIndex + 1), characterToPrint).rjust(stepWidth * numberOfLines)
@@ -110,6 +120,7 @@ def printPyramid(printCommand, numberOfLines, characterToPrint, stepWidth):
 
 
 def printSimplePyramids():
+    """Loop for interacting with simple pyramids"""
     while True:
         print("Pyramid: Simple")
         printCommand = selectPrintPyramidCommand()
